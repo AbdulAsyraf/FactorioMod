baa9d576ba65b9ca690c32278b5efc58ee43c5a8
AbdulAsyraf <abdulasyrafah@gmail.com>
2019-07-08 19:47:37 +0800

    rigged things to produce full belts

--- prototypes/entity/resources.lua
+++ prototypes/entity/resources.lua
@@ -1,4 +1,4 @@
-data.raw["resource"]["iron-ore"].minable.count = 10
-data.raw["resource"]["copper-ore"].minable.count = 10
-data.raw["resource"]["coal"].minable.count = 10
-data.raw["resource"]["stone"].minable.count = 10
+data.raw["resource"]["iron-ore"].minable.count = 11
+data.raw["resource"]["copper-ore"].minable.count = 11
+data.raw["resource"]["coal"].minable.count = 11
+data.raw["resource"]["stone"].minable.count = 11
--- prototypes/recipe/furnace.lua
+++ prototypes/recipe/furnace.lua
@@ -1,11 +1,15 @@
-data.raw.recipe["iron-plate"].result_count = 8
-data.raw.recipe["iron-plate"].energy_required = 0.8
+data.raw.recipe["iron-plate"].result_count = 15
+local ironEnergy = data.raw.recipe["iron-plate"].energy_required
+data.raw.recipe["iron-plate"].energy_required = ironEnergy/4
 
-data.raw.recipe["copper-plate"].result_count = 8
-data.raw.recipe["copper-plate"].energy_required = 0.8
+data.raw.recipe["copper-plate"].result_count = 15
+local copperEnergy = data.raw.recipe["copper-plate"].energy_required
+data.raw.recipe["copper-plate"].energy_required = copperEnergy/4
 
-data.raw.recipe["steel-plate"].result_count = 5
-data.raw.recipe["steel-plate"].energy_required = 4
+data.raw.recipe["steel-plate"].normal.result_count = 15
+local steelEnergy = data.raw.recipe["steel-plate"].normal.energy_required
+data.raw.recipe["steel-plate"].normal.energy_required = steelEnergy/4
 
-data.raw.recipe["stone-brick"].result_count = 8
-data.raw.recipe["stone-brick"].energy_required = 0.8
+data.raw.recipe["stone-brick"].result_count = 15
+local stoneEnergy = data.raw.recipe["stone-brick"].energy_required
+data.raw.recipe["stone-brick"].energy_required = stoneEnergy/4
